e8ebfe6106de7839f29d06e61ce99057e85ef635
Henriquebap <henriquebaptista2003@gmail.com>
2023-02-24 21:05:42 -0300

    heritage, Generalization/Specialization and Methods Using 'super()'

--- main.py
+++ main.py
@@ -1,55 +1,44 @@
-class Filme:
+class Program:
+    def __init__(self, nome, ano):
+        self._name = nome.title()
+        self.year = ano
+        self._likes = 0
+
+    @property
+    def like(self):
+        return self._likes
+
+    def give_likes(self):
+        self._likes += 1
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name.title()
+
+
+class Filme(Program):
     def __init__(self, nome, ano, duracao):
-        self.__name = nome.title()
-        self.year = ano
+        super().__init__(nome, ano)
         self.time = duracao
-        self.__like = 0
-
-    @property
-    def like(self):
-        return self.__like
-
-    def give_likes(self):
-        self.__like += 1
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, new_name):
-        self.__name = new_name.title()
 
 
-class Serie:
+class Serie(Program):
     def __init__(self, nome, ano, temporadas):
-        self.__name = nome.title()
-        self.year = ano
+        super().__init__(nome, ano)
         self.time = temporadas
-        self.__like = 0
-
-    @property
-    def like(self):
-        return self.__like
-
-    def give_likes(self):
-        self.__like += 1
-
-    @property
-    def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, new_name):
-        self.__name = new_name.title()
 
 
 vingadores = Filme('vingadores - Infinity war', 2018, 160)
 vingadores.give_likes()
 
 print(
-    f'Nome: {vingadores.name} - Ano {vingadores.year} - Duracao do filme {vingadores.time} - Cutidas {vingadores.like}')
+    f'{vingadores.name} - {vingadores.year} - {vingadores.time} - {vingadores.like}')
 
 dark = Serie('Dark', 2018, 4)
 dark.give_likes()
-print(f'Nome: {dark.name} - Ano: {dark.year}- Temporadas: {dark.time} - Curtidas: {dark.like}')
+print(
+    f'{dark.name} - {dark.year} - {dark.time} - {dark.like}')
